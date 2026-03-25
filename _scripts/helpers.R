358f2661afe0384168aecfc6e50ff891dfa27802
hebstr <ju.diez@protonmail.com>
2026-03-25 02:22:49 +0100

    .

---
+++ _scripts/helpers.R
@@ -0,0 +1,19 @@
+glue_qmd <- \(x, .envir = parent.frame()) {
+  stringr::str_glue(x, .open = "<<", .close = ">>", .envir = .envir)
+}
+
+social_icon <- \(id, icon, set = "fa6-brands") {
+  glue_qmd("
+    <a
+      href='{{< var links.<<id>>.href >}}'
+      title=<<id>>
+      target='_blank'
+      rel='noopener'>
+      <iconify-icon
+        icon='<<set>>:<<icon>>'
+        width='1.5em'
+        height='1.5em'>
+      </iconify-icon>
+    </a>
+  ")
+}
